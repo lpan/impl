069a19e347fa1e64f9aea0d7766311121d3ee9df
Lawrence Pan <lawrencefeipan@gmail.com>
2018-05-27 20:28:14 -0400

    remove duplicated test

--- ds/heap_test.go
+++ ds/heap_test.go
@@ -10,27 +10,6 @@ func prepend(slice []int, i int) []int {
 	return append([]int{i}, slice...)
 }
 
-func Test_HeapSort(t *testing.T) {
-	h := DefaultHeap()
-	nums := st.Input(15)
-
-	for _, n := range nums {
-		h.Insert(n)
-	}
-
-	var result []int
-	for {
-		n, ok := h.DeleteMax()
-		if !ok {
-			break
-		}
-
-		result = prepend(result, n)
-	}
-
-	st.AssertSorted(t, result)
-}
-
 func Test_BuildHeapFixUp(t *testing.T) {
 	nums := st.Input(15)
 	h := NewHeapBad(nums)
