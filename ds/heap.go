5f1a609a9a093f74e7ec38286ab82f3f8df8a53b
Lawrence Pan <lawrencefeipan@gmail.com>
2018-05-27 20:27:26 -0400

    implement heapify

--- ds/heap.go
+++ ds/heap.go
@@ -1,7 +1,30 @@
 package ds
 
-// NewHeap constructs an empty Heap
-func NewHeap() Heap {
+// NewHeapBad naively constructs a heap based on the given list of integers
+// Worst-case running time: Theta(nlogn)
+func NewHeapBad(ns []int) Heap {
+	h := &heap{}
+	for _, n := range ns {
+		h.Insert(n)
+	}
+	return h
+}
+
+// NewHeap constructs a heap using heapify
+// Worst-case running time: Theta(n)
+func NewHeap(ns []int) Heap {
+	bt := bintree(ns)
+	h := &heap{bt: bt}
+
+	for i := len(bt) - 1; i >= 0; i-- {
+		h.fixDown(i)
+	}
+
+	return h
+}
+
+// DefaultHeap constructs an empty Heap
+func DefaultHeap() Heap {
 	return &heap{}
 }
 
@@ -46,27 +69,38 @@ func (h *heap) DeleteMax() (int, bool) {
 	last := h.bt[h.bt.lastIndex()]
 	h.bt[0] = last
 
-	h.fixDown()
+	// start fix down at root
+	h.fixDown(0)
 
 	h.bt = h.bt[:len(h.bt)-1]
 	return max, true
 }
 
-func (h *heap) fixDown() {
+func (h *heap) fixDown(start int) {
 	var pi, ci int
+	pi = start
 	for {
 		li, ok := h.bt.leftChildIndex(pi)
+
+		// if no child, do nothing
 		if !ok {
 			break
 		}
 
 		ri, ok := h.bt.rightChildIndex(pi)
+
+		// locate the largest child
 		if !ok {
 			ci = li
 		} else if h.bt[ri] > h.bt[li] {
 			ci = ri
 		} else {
 			ci = li
+		}
+
+		// if parent is larger than the largest child, do nothing
+		if h.bt[pi] >= h.bt[ci] {
+			break
 		}
 
 		h.bt[pi], h.bt[ci] = h.bt[ci], h.bt[pi]
--- ds/heap_test.go
+++ ds/heap_test.go
@@ -11,12 +11,46 @@ func prepend(slice []int, i int) []int {
 }
 
 func Test_HeapSort(t *testing.T) {
-	h := NewHeap()
+	h := DefaultHeap()
 	nums := st.Input(15)
 
 	for _, n := range nums {
 		h.Insert(n)
 	}
+
+	var result []int
+	for {
+		n, ok := h.DeleteMax()
+		if !ok {
+			break
+		}
+
+		result = prepend(result, n)
+	}
+
+	st.AssertSorted(t, result)
+}
+
+func Test_BuildHeapFixUp(t *testing.T) {
+	nums := st.Input(15)
+	h := NewHeapBad(nums)
+
+	var result []int
+	for {
+		n, ok := h.DeleteMax()
+		if !ok {
+			break
+		}
+
+		result = prepend(result, n)
+	}
+
+	st.AssertSorted(t, result)
+}
+
+func Test_BuildHeapHeapify(t *testing.T) {
+	nums := st.Input(7)
+	h := NewHeap(nums)
 
 	var result []int
 	for {
