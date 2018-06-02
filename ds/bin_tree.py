e6cdadf49f4f6f93cbee6c70435cb2829ee3bd82
Lawrence Pan <lawrencefeipan@gmail.com>
2018-06-02 15:41:18 -0400

    add postorder traversal + move docstring inside fns

--- ds/bin_tree.py
+++ ds/bin_tree.py
@@ -31,11 +31,12 @@ def traverser(traverse_fn):
 
     return wrapper
 
-"""
-visit node before push children on to the stack
-"""
 @traverser
 def preorder(tree, visit):
+    """
+    visit node before push children on to the stack
+    """
+
     def traverse(node):
         if node:
             visit(node)
@@ -44,13 +45,14 @@ def preorder(tree, visit):
 
     traverse(tree)
 
-"""
-1. visit node
-2. push children on the stack (left on top of right tho)
-3. pop stack and go back to #1 until stack is empty
-"""
 @traverser
 def preorder_iter(tree, visit):
+    """
+    1. visit node
+    2. push children on the stack (left on top of right tho)
+    3. pop stack and go back to #1 until stack is empty
+    """
+
     stack = [tree]
     while stack:
         node = stack.pop()
@@ -59,14 +61,14 @@ def preorder_iter(tree, visit):
             stack.append(node.right)
             stack.append(node.left)
 
-"""
-visit node right before stack starts to unwind
-1. reach to the deepest left node
-2. visit the node as stack frames being popped
-3. push right nodes onto the stack
-"""
 @traverser
 def inorder(tree, visit):
+    """
+    inorder visits the left-most leaf node first, then root, then right sub-tree
+
+    visit the node after its left child's stack frame is popped
+    """
+
     def traverse(node):
         if node:
             traverse(node.left)
@@ -75,15 +77,16 @@ def inorder(tree, visit):
 
     traverse(tree)
 
-"""
-1. reach the left-most leaf of the root, push nodes onto the stack along the
-way
-2. pop the stack, visit it
-3. go to step #1, treat the right child of the node as the root
-4. when stack is empty, return
-"""
 @traverser
 def inorder_iter(tree, visit):
+    """
+    1. reach the left-most leaf of the root, push nodes onto the stack along the
+    way
+    2. pop the stack, visit it
+    3. go to step #1, treat the right child of the node as the root
+    4. when stack is empty, return
+    """
+
     stack = []
     root = tree
 
@@ -100,9 +103,49 @@ def inorder_iter(tree, visit):
 
         root = node.right
 
+@traverser
+def postorder(tree, visit):
+    """
+    postorder means visiting the root last.
+    visit left, right, root
+
+    visit node when the stack frame of the node is about to be popped
+    """
+
+    def traverse(node):
+        if node:
+            traverse(node.left)
+            traverse(node.right)
+            visit(node)
+
+    traverse(tree)
+
+@traverser
+def postorder_iter(tree, visit):
+    """
+    https://www.geeksforgeeks.org/iterative-postorder-traversal/
+
+    basically it is a reverse of preorder...
+    """
+
+    stack = [tree]
+    result = []
+
+    while stack:
+        node = stack.pop()
+        if node:
+            stack.append(node.left)
+            stack.append(node.right)
+            result.append(node)
+
+    while result:
+        visit(result.pop())
+
 if __name__ == "__main__":
     tree = default_tree()
     print("preorder:", preorder(tree))
     print("preorder:", preorder_iter(tree))
     print("inorder:", inorder(tree))
     print("inorder:", inorder_iter(tree))
+    print("postorder:", postorder(tree))
+    print("postorder:", postorder_iter(tree))
